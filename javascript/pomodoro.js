c4de9209fbc50a04fc88052c0675c5ed90a9502c
Pariya Tavangar <ptavangar1382@gmail.com>
2024-09-14 15:48:36 +0330

    pomodoro-btn-styles

--- javascript/pomodoro.js
+++ javascript/pomodoro.js
@@ -109,9 +109,32 @@
 
 //--------- modal ---------------
 
-// const myModal = document.getElementById('myModal')
-// const myInput = document.getElementById('myInput')
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
 
-// myModal.addEventListener('shown.bs.modal', () => {
-//   myInput.focus()
-// })
+// close modal function
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// open modal function
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+// open modal event
+openModalBtn.addEventListener("click", openModal);
