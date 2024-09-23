c8b6d879e518dfda8afb1c31a5e14b0e61490c6a
Pariya Tavangar <ptavangar1382@gmail.com>
2024-09-23 05:25:39 +0330

    idk-im-drunk

--- javascript/pomodoro.js
+++ javascript/pomodoro.js
@@ -118,6 +118,66 @@ var timerpanel = document.getElementById("timer-panel");
 var themepanel = document.getElementById("theme-panel");
 const hidebtn = document.getElementById("hide-btn")
 
+let imgBg = document.querySelector("#img-list");
+let colorBg = document.querySelector('#color-list');
+
+
+imgBg.addEventListener('change',function(){
+  if (imgBg.value=='default-img'){
+      document.body.style.backgroundColor='';
+      document.body.style.backgroundImage='url(../img/bg.jpg)';
+  }
+  else if (imgBg.value=='cozy-room'){
+      document.body.style.backgroundColor='';
+      document.body.style.backgroundImage='url(../img/cozy.jpeg)';
+  }
+  else if (imgBg.value=='fireplace'){
+      document.body.style.backgroundColor='';
+      document.body.style.backgroundImage='url(../img/fireplace.jpg)';
+  }
+  else if (imgBg.value=='nature'){
+      document.body.style.backgroundColor='';
+      document.body.style.backgroundImage='url(../img/nature.jpg)';
+  }
+
+})
+
+
+colorBg.addEventListener('change',function(){
+  if (colorBg.value=='red'){
+    document.body.style.backgroundImage='none';
+    document.body.style.backgroundColor='#db3545';
+  }
+  else if (colorBg.value=='orange'){
+    document.body.style.backgroundImage='none';
+    document.body.style.backgroundColor='#fc9642';
+}
+else if (colorBg.value=='yellow'){
+    document.body.style.backgroundImage='none';
+    document.body.style.backgroundColor='#ffcd38';
+}
+else if (colorBg.value=='green'){
+    document.body.style.backgroundImage='none';
+    document.body.style.backgroundColor='#1a8754';
+}
+else if (colorBg.value=='blue'){
+    document.body.style.backgroundImage='none';
+    document.body.style.backgroundColor='#0d6dfc';
+}
+else if (colorBg.value=='green'){
+    document.body.style.backgroundImage='none';
+    document.body.style.backgroundColor='#1a8754';
+}
+else if (colorBg.value=='purple'){
+    document.body.style.backgroundImage='none'; 
+    document.body.style.backgroundColor='#8b68cc';
+}
+else if (colorBg.value=='gray'){
+    document.body.style.backgroundImage='none';
+    document.body.style.backgroundColor='#6b757d';
+}
+})
+
 
 document.addEventListener("click",hidebtn)
 {
